bec8836fa26348acbfa89958b318cc19bc12e6d6
kvetals <kvetals@gmail.com>
2015-07-18 15:06:31 +0300

    added some math operations

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,16 @@
 public class Calculate{
 	public static void main(String[] args){
 		System.out.println("Calculate");
+		int first = Integer.valueOf(args[0]);
+		int second = Integer.valueOf(args[1]);
+		int sum = first + second;
+		int dif = first - second;
+		int mult = first * second;
+		int dev = first / second;
+		System.out.println("Sum = " + sum);
+		System.out.println("Dif = " + dif);
+		System.out.println("Mult = " + mult);
+		System.out.println("Dev = " + dev);
+		
 	}
 }
